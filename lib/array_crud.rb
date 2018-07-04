465eabc6df0b253629008f2a100b2223ab0bacee
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-04 16:11:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,35 @@
 def create_an_empty_array
+ array = []
   
 end
 
 def create_an_array
   
+  array = ["this","is", "an","array"]
+  
 end
 
 def add_element_to_end_of_array(array, element)
+  array = ["stuff", "things"]
+  array.push("arrays!")
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["stuff", "things"]
+  array.unshift("wow")
   
 end
 
 def remove_element_from_end_of_array(array)
+  array = ["stuff", "things", "arrays!"]
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  array = ["stuff", "things", "arrays!"]
+  array.pop
   
 end
 
